37c129028355bcd61ba29a8906fae83ce4aa8704
Javon Harper <javon.d.harper@gmail.com>
2013-05-30 00:04:34 -0400

    Add default values to letter

--- app/assets/javascripts/models/letter.js
+++ app/assets/javascripts/models/letter.js
@@ -1,6 +1,6 @@
 App.Letter = DS.Model.extend({
-  author: DS.attr('string'),
-  pitch: DS.attr('string'),
-  ask: DS.attr('string'),
+  author: DS.attr('string', { defaultValue: 'Timmy'}),
+  pitch: DS.attr('string', {  defaultValue: 'I have really tried to be good this year.'}),
+  ask: DS.attr('string', { defaultValue: 'Please bring me'}),
   gifts: DS.hasMany('App.Gift')
 });
