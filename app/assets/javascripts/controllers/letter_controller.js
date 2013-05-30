46b9b40fd939bf6ac7be8defc35e7f46d615715d
Javon Harper <javon.d.harper@gmail.com>
2013-05-30 00:04:47 -0400

    Have attributes changing based off model, add and remove gifts

--- app/assets/javascripts/controllers/letter_controller.js
+++ app/assets/javascripts/controllers/letter_controller.js
@@ -1,44 +1,22 @@
 App.LetterController = Em.ObjectController.extend({
-  newGift: null,
-  gifts:   null,
-
-  yourName: 'Timmy',
-  pitch:    'I have really tried to be good this year.',
-  ask:      'Please bring me',
-
-  reset: function () {
-    var gift = App.Gift.createRecord();
-    this.set('newGift', gift);
-  },
-
-  add: function () {
+  addGift: function() {
     var gift = this.get('newGift');
+
     if (gift.get('name')) {
       this.get('gifts').addObject(gift);
-      gift.get('transaction').commit();
       this.reset();
     }
   },
 
-  deleteGift: function (gift) {
-    transaction = this.get('store').transaction();
-    transaction.add(gift);
-    gift.deleteRecord();
-    transaction.commit();
+  reset: function() {
+    this.set('newGift', App.Gift.createRecord());
+  },
+
+  sendToSanta: function() {
+    console.log('should persist this');
+  },
+
+  deleteGift: function(gift) {
     this.get('gifts').removeObject(gift);
-  },
-
-  deleteRecords: function () {
-    transaction = this.get('store').transaction();
-    this.get('gifts').forEach(function (gift) {
-      transaction.add(gift);
-      gift.deleteRecord();
-    });
-    transaction.commit();
-  },
-
-  sendToSanta: function () {
-    this.deleteRecords();
-    this.transitionToRoute('thank_you');
   }
 });
--- app/assets/javascripts/routes/letter_route.js
+++ app/assets/javascripts/routes/letter_route.js
@@ -1,6 +1,7 @@
 App.LetterRoute = Em.Route.extend({
-  setupController: function(){
-    this.controller.set('gifts', App.Gift.find({}))
-    this.controller.reset();
+  setupController: function(controller) {
+    controller.transaction = controller.get('store').transaction();
+    controller.set('content', controller.transaction.createRecord(App.Letter, {}));
+    controller.set('newGift', controller.transaction.createRecord(App.Gift, {}));
   }
 });
